9d408583bf40dfc5e9da2749270589a5429927f8
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-14 12:59:20 -0500

    Add POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var {mongoose} = require("./db/mongoose");
+var {Todo} = require("./models/todo");
+var {User} = require("./models/user");
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+    .then((doc) => {
+        res.send(doc);
+    })
+    .catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
