e33e49940a2f26f061d4aa053dc845854a13971b
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-18 18:49:35 -0500

    Add Heroku configurations

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+//mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://hernandezramiro:123ramirin546**@ds149603.mlab.com:49603/rshtodosapp");
 
 module.exports = {mongoose};
