103d1c8b5e11b6974cd74c58bc2110eabc2632b2
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-18 19:33:36 -0500

    Delete some bad data

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,5 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
-//mongoose.connect("mongodb://hernandezramiro:654ramiro321@ds149603.mlab.com:49603/rshtodosapp");
 
 module.exports = {mongoose};
