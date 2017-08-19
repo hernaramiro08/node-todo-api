1e790681eebe8abb0388fdc78809cc72f5c28171
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-18 19:10:13 -0500

    Set MongoDB credentials

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-//mongoose.connect("mongodb://localhost:27017/TodoApp");
-mongoose.connect("mongodb://hernandezramiro:123ramirin546**@ds149603.mlab.com:49603/rshtodosapp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+//mongoose.connect("mongodb://hernandezramiro:123ramirin546**@ds149603.mlab.com:49603/rshtodosapp");
 
 module.exports = {mongoose};
