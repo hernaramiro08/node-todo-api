4a659e2f2528bf2d3e9b1a5c5834286a79d0afa4
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-18 19:31:45 -0500

    Set MongoDB credentials to connect from Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
-//mongoose.connect("mongodb://hernandezramiro:123ramirin546**@ds149603.mlab.com:49603/rshtodosapp");
+//mongoose.connect("mongodb://hernandezramiro:654ramiro321@ds149603.mlab.com:49603/rshtodosapp");
 
 module.exports = {mongoose};
