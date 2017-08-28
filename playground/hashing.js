1d958fe659fed01ae4bb587990148b1359aedd9d
ramiro.hernandez <urcoelmaldito@gmail.com>
2017-08-28 18:37:12 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,22 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+var password= "123abc!";
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = "$2a$10$D7uNrCz6CBEUFqo7xD/UUez/fku4m2rOblpS1.Wo3DODEv5Ni9AJi";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*
 var data = {
     id: 10
 };
@@ -10,7 +26,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, "123abc");
 console.log("decoded", decoded);
-
+*/
 
 /*
 var message = "I am user number 3";
